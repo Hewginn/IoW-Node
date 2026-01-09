591c5d34d6e16d32a1bb0a9396ea4a2a3cf8fe1a
Hewginn <somogyizolt@gmail.com>
2026-01-09 12:54:58 +0100

    picamera2 2

--- camera_control/CameraControl.py
+++ camera_control/CameraControl.py
@@ -1,15 +1,15 @@
-import picamera2
+from picamera2 import Picamera2
 
 class Camera:
 
     # Initiate camera using PICAMERA libary
     def __init__(self):
-        self.camera = picamera2.PiCamera2()
-        self.camera.configure(picamera2.create_still_configuration())
+        self.camera = Picamera2()
+        self.capture_config = self.camera.create_still_configuration()
     
     # Capture one image
     def takePicture(self, jpg_file: str):
 
-        picamera2.start()
-        picamera2.capture_file(jpg_file)
-        picamera2.stop()
+        self.camera.start()
+        self.camera.switch_mode_and_capture_file(self.capture_config, jpg_file)
+        self.camera.stop()
