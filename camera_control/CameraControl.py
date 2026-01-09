1ca824c7fbc03c490b30be3a17c7c62ae4364864
Hewginn <somogyizolt@gmail.com>
2026-01-09 14:54:21 +0100

    camera fix

--- camera_control/CameraControl.py
+++ camera_control/CameraControl.py
@@ -1,15 +1,17 @@
 from picamera2 import Picamera2
+import time
 
 class Camera:
 
     # Initiate camera using PICAMERA libary
     def __init__(self):
         self.camera = Picamera2()
-        self.capture_config = self.camera.create_still_configuration()
+        self.camera.configure(self.camera.create_still_configuration())
     
     # Capture one image
     def takePicture(self, jpg_file: str):
 
         self.camera.start()
-        self.camera.switch_mode_and_capture_file(self.capture_config, jpg_file)
+        time.sleep(1)
+        self.camera.capture_file(jpg_file)
         self.camera.stop()
