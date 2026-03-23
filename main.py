e5cbee3a57be330d64fbed11c69b0ca295154b73
Hewginn <somogyizolt@gmail.com>
2026-03-23 18:55:37 +0100

    MainController

--- main.py
+++ main.py
@@ -4,102 +4,120 @@ from communication_control import SessionControl
 from camera_control import CameraControl
 import time
 
-# Initiating Node
-node = NodeControl.Node(
-    NODE_NAME,
-    NODE_PASSWORD,
-    NODE_STATUS,
-    NODE_LOCATION,
-    NODE_MAIN_UNIT
-)
+class MainController:
+    def __init__(self):
 
-# Initiating Sensors
-sensors: list[SensorControl.Sensor] = [
-    SensorControl.DHT11(SENSORS["DHT11"]),
-    SensorControl.GUVAS12SD(SENSORS["GUVAS12SD"])
-]
+        #Initiating Node
+        self.node = NodeControl.Node(
+            NODE_NAME,
+            NODE_PASSWORD,
+            NODE_STATUS,
+            NODE_LOCATION,
+            NODE_MAIN_UNIT
+        )
 
-# Initiating "Session"
-session = SessionControl.NodeSessionControl(SERVER_URL)
+        # Initiating Sensors
+        self.sensors: list[SensorControl.Sensor] = [
+            SensorControl.DHT11(SENSORS["DHT11"]),
+            SensorControl.GUVAS12SD(SENSORS["GUVAS12SD"])
+        ]
 
-# Initiating camera
-cameras: list[CameraControl.Camera] = [
-    CameraControl.RaspberryPiCameraModuleV2(CAMERAS["RaspberryPi Camera Module V2"])
-]
+        # Initiating "Session"
+        self.session = SessionControl.NodeSessionControl(SERVER_URL)
 
-# Cycle counter
-number_of_cycles = 0
+        # Initiating camera
+        self.cameras: list[CameraControl.Camera] = [
+            CameraControl.RaspberryPiCameraModuleV2(CAMERAS["RaspberryPi Camera Module V2"])
+        ]
 
-while(True):
+        self.is_node_faulty = False
 
-    # Trying to connect to server
-    if not session.isAuth:
+        self.counter = 0
 
-        # Connecting to server and setting sensor and node values
-        try:
-            session.connect(PAGE_CONNECT, node.connect())
-            if session.isAuth:
-                node.setState(NODE_STATUS)
-                node.server_control = session.updateNode(PAGE_NODE, node.details())
-                for sensor in sensors:
-                    session.send(PAGE_SENSOR, sensor.details())
-                for camera in cameras:
-                    session.send(PAGE_CAMERA, camera.details())
-        except Exception as e:
-            node.setState(NodeControl.NodeState.Offline)
-    else:
-        # Setting node values and getting the server control
-        node.server_control = session.updateNode(PAGE_NODE, node.details())
 
-    # Sending data if the Node is not offline and the server approves
-    if node.status != NodeControl.NodeState.Offline and node.server_control:
+    def connecting(self):
+        # Trying to connect to server
+        if not self.session.isAuth:
 
-        is_node_faulty = False
+            # Connecting to server and setting sensor and node values
+            try:
+                self.session.connect(PAGE_CONNECT, self.node.connect())
+                if self.session.isAuth:
+                    self.node.setState(NODE_STATUS)
+                    self.node.server_control = self.session.updateNode(PAGE_NODE, self.node.details())
+                    for sensor in self.sensors:
+                        self.session.send(PAGE_SENSOR, sensor.details())
+                    for camera in self.cameras:
+                        self.session.send(PAGE_CAMERA, camera.details())
+            except Exception as e:
+                print(f"Exception error: {e}")
+                self.node.setState(NodeControl.NodeState.Offline)
+        else:
+            # Setting node values and getting the server control
+            self.node.server_control = self.session.updateNode(PAGE_NODE, self.node.details())
 
+    def measurement(self):
         # Gathering sensor data
         data = []
-        for sensor in sensors:
-
+        for sensor in self.sensors:
             # Getting the sensor data and adding to other data
             sensor_data = sensor.getData()
             data += sensor_data
+            # Sending sensor info if the status changed
+            if sensor.changed_state:
+                self.session.send(PAGE_SENSOR, sensor.details())
+                sensor.changed_state = False
+            self.is_node_faulty = self.is_node_faulty or not sensor.is_online
+        print(data)
+        # Sending HTTP post of sensors
+        for item in data:
+            self.session.send(PAGE_DATA, item)
+
+    def camera(self):
+
+        for camera in self.cameras:
+
+            # Getting and sending the image
+            image_message = camera.getImageMessage()
+            self.session.sendImage(PAGE_IMAGE, camera.path, image_message)
 
             # Sending camera info if the status changed
-            if sensor.changed_state:
-                session.send(PAGE_SENSOR, sensor.details())
-                sensor.changed_state = False
-            is_node_faulty = is_node_faulty or not sensor.is_online
+            if camera.changed_state:
+                self.session.send(PAGE_CAMERA, camera.details())
+                camera.changed_state = False
 
-        print(data)
-
-        # Sending HTTP post of sensors
-        for data in data:
-            session.send(PAGE_DATA, data)
-
-        # Sending image to server
-        if number_of_cycles % 5 == 0:
-
-            for camera in cameras:
-
-                # Getting and sending the image
-                image_message = camera.getImageMessage()
-                session.sendImage(PAGE_IMAGE, camera.path, image_message)
-
-                # Sending camera info if the status changed
-                if camera.changed_state:
-                    session.send(PAGE_CAMERA, camera.details())
-                    camera.changed_state = False
-
-                # The node is faulty if one camera is faulty
-                is_node_faulty = is_node_faulty or not camera.is_online
-
+    def changeState(self):
         # Setting faulty state if one sensor or camera is offline
-        if is_node_faulty:
-            node.setState(NodeControl.NodeState.Faulty)
+        if self.is_node_faulty:
+            self.node.setState(NodeControl.NodeState.Faulty)
         else:
-            node.setState(NODE_STATUS)
-    
-    # Incrementing number of cycles
-    number_of_cycles += 1
+            self.node.setState(NODE_STATUS)
 
-    time.sleep(DATA_SEND_FRQ)
+    def run(self):
+
+        self.is_node_faulty = False
+        self.connecting()
+
+        # Sending data if the Node is not offline and the server approves
+        if self.node.status != NodeControl.NodeState.Offline and self.node.server_control:
+            self.measurement()
+            if self.counter % IMG_SEND_COUNTER == 0:
+                self.camera()
+            for camera in self.cameras:
+                self.is_node_faulty = self.is_node_faulty or not camera.is_online
+            self.changeState()
+        
+        self.counter = self.counter + 1
+
+def main():
+    main_controller = MainController()
+    while(True):
+        try:
+            main_controller.run()
+        except Exception as e:
+            print(f"Runtime error: {e}")
+        time.sleep(DATA_SEND_FRQ)
+
+if __name__ == "__main__":
+    main()
+
