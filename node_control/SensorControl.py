da60befef38e8e7e1d6c0688244a500ad40c525d
Hewginn <somogyizolt@gmail.com>
2026-04-09 18:55:04 +0200

    uv

--- node_control/SensorControl.py
+++ node_control/SensorControl.py
@@ -97,7 +97,9 @@ class GUVAS12SD(Sensor):
     def readData(self):
         try:
             # Calibration of measured voltage
-            self.uv_intensity = abs((self.chan.voltage + 0.00025) * 2.09) # mW/cm2
+            self.uv_intensity = (self.chan.voltage + 0.00025) * 2.09 # mW/cm2
+            if self.uv_intensity < 0:
+                self.uv_intensity = self.uv_intensity * -1
             if self.is_online == False:
                 self.is_online = True
                 self.changed_state = True
